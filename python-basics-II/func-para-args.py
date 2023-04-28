2a4d433e6b8c1555b12acb13f5a88f5fd87aa290
josewebdev2000 <josewebdev@proton.me>
2023-04-28 18:14:48 -0400

    Function Parameters V/S Function Arguments

---
+++ python-basics-II/func-para-args.py
@@ -0,0 +1,9 @@
+# Function parameters V/S Function arguments
+
+#Function Parameters
+
+def say_hello(name, emoji):
+    print(f"Hello {name} {emoji}")
+
+# Function Arguments
+say_hello("Jose", ":)")
