52d9e7d535fcd3d82e2eed60b0cd0ea1a5611f08
josewebdev2000 <josewebdev@proton.me>
2023-04-30 13:00:13 -0400

    Decorators Exercise

---
+++ exercises/authenticated/main.py
@@ -0,0 +1,31 @@
+# Create an @authenticated decorator that only allows the function to run if user1 has 'valid' set to True
+
+def main():
+    
+    user1 = {
+    "name": "Sorna",
+    "valid": True # changing this will either run or not run the message_friends function
+    }
+    
+    user2 = {
+        "name": "Luigi",
+        "valid": False
+    }
+
+    message_friends(user1)
+    message_friends(user2)
+
+def authenticated(fn):
+    # code here
+    def wrapper_func(user):
+        if user["valid"]:
+            fn(user)
+    
+    return wrapper_func
+
+@authenticated
+def message_friends(user):
+    print(f"message has been sent to {user.get('name')}")
+
+if __name__ == "__main__":
+    main()
